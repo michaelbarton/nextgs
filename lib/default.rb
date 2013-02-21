6a54cc940006ec562a8c88629d97711ca44c384b
Michael Barton <mail@michaelbarton.me.uk>
2013-02-21 18:12:37 -0500

    Reorganise lib files

--- lib/default.rb
+++ lib/default.rb
@@ -1,14 +1,2 @@
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
-
-def highlight
-  "<pre class=\"class=prettyprint\">"
-end
-
-def endhighlight
-  "</pre>"
-end
-
-def image(url,width=400)
-  "<img src='#{relative_path_to(url)}' width=#{width}>"
-end
